8ffd629a0d673086f9ca1082b9a20a75eb37565b
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2022-09-27 17:19:54 -0400

    Basic_Node_And_React_App

---
+++ blog-miniProject/client/src/App.js
@@ -0,0 +1,18 @@
+
+import PostCreate from './PostCreate';
+import PostList from './PostList';
+
+function App() {
+  return (
+    <div className="container">
+       <h1>Microservices Blog App (Mini Project)</h1>
+      <h2>Create Post</h2>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
