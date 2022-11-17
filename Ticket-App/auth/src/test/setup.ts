ec606c46378f6e6adab0def8c573ca3c56414d6e
Maitrik Patel <59853047+maitrikpatel2025@users.noreply.github.com>
2022-11-16 21:02:26 -0500

    auth-testing-215-checkpoint

--- Ticket-App/auth/src/test/setup.ts
+++ Ticket-App/auth/src/test/setup.ts
@@ -1,6 +1,11 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose, { ConnectOptions } from 'mongoose';
 import { app } from '../app';
+
+declare global {
+  var signin :() => Promise<string[]>
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -30,3 +35,20 @@ afterAll(async () => {
     }
     await mongoose.connection.close();
   });
+
+
+global.signin =async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+    
+    const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password
+    })
+    .expect(201)
+
+    const cookie =response.get('Set-Cookie');
+    return cookie
+  }
